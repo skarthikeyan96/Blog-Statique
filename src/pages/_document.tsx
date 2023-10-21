5bab96efb4feac04caf4ee90986980e8390a366b
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-10-22 04:47:22 +0530

    feat: add initial ui component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Theme } from '@radix-ui/themes';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+      <Theme>
+
         <Main />
+        </Theme>
         <NextScript />
       </body>
     </Html>
---
+++ src/pages/generate-stats.tsx
@@ -0,0 +1,98 @@
+import { Input } from "@/components/ui/input";
+import { Container, Flex } from "@radix-ui/themes";
+import Select from "react-tailwindcss-select";
+import Link from "next/link";
+
+import themes from "@/helper";
+
+const GenerateStats = () => {
+  const getThemes = () => {
+    return Object.keys(themes).map((theme: string) => {
+      return {
+        value: theme,
+        label: theme.replace("_", " "),
+      };
+    });
+  };
+
+  const getYears = () => {
+    const endYear = new Date().getFullYear();
+    const years = [];
+    let startYear = new Date().getFullYear() - 10;
+    while (startYear <= endYear) {
+      years.push(startYear++);
+    }
+
+    return years.map((year: number) => {
+      return {
+        value: year.toString(),
+        label: year.toString(),
+      };
+    });
+  };
+
+  return (
+    <>
+      <nav className="shadow-sm py-8 px-8 md:px-4">
+        <Container>
+          <Flex justify="between">
+            <Link href="/generate-stats" className="font-bold text-lg">
+              {" "}
+              Blog stats{" "}
+            </Link>
+            <Link href="/" className="text-gray-900">
+              {" "}
+              Homepage{" "}
+            </Link>
+          </Flex>
+        </Container>
+      </nav>
+      <Container className="mt-10">
+        <h1 className="text-3xl font-bold mb-10">Generate your blog stats</h1>
+        <div className="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 mt-10">
+          <div className="col-span-1">
+            <div className="flex flex-col gap-4">
+              <div className="space-y-2">
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  {" "}
+                  Dev.to username{" "}
+                </label>
+                <Input type="text" placeholder="Dev.to Username" />
+              </div>
+
+              <div className="space-y-2">
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  {" "}
+                  Choose a theme{" "}
+                </label>
+
+                <Select
+                  options={getThemes()}
+                  onChange={(value) => console.log(value)}
+                  value={null}
+                  primaryColor={""}
+                />
+              </div>
+
+              <div>
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  {" "}
+                  Choose a year{" "}
+                </label>
+
+                <Select
+                  options={getYears()}
+                  onChange={(value) => console.log(value)}
+                  value={null}
+                  primaryColor={""}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default GenerateStats;
